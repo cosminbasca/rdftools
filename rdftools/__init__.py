63a1e3bcef8dc178c370275aa73db74a1e5331be
Cosmin Basca <cosmin.basca@gmail.com>
2014-04-08 13:13:00 +0000

    bumped to version 0.3.3
    - now testing ...

--- rdftools/__init__.py
+++ rdftools/__init__.py
@@ -2,11 +2,7 @@ __author__ = 'Cosmin Basca'
 __email__ = 'basca@ifi.uzh.ch; cosmin.basca@gmail.com'
 
 from .raptorutil import *
-from .lubm import *
-from .rdf2rdf import *
-from .nxparser import *
-from .rdftools.tools.void import *
 from .util import *
 from .rdfparse import *
-from .rdftools.tools.encoder import *
 from .gcityhash import *
+from .tools import *
--- rdftools/__version__.py
+++ rdftools/__version__.py
@@ -1,4 +1,4 @@
 __author__ = 'basca'
 
-version = (0, 3, 2)
+version = (0, 3, 3)
 str_version = '.'.join(['%s' % v for v in version])
