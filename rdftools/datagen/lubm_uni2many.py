fca8787264bbefd3fbc24ff0c335b7257b007e6a
Cosmin Basca <cosmin.basca@gmail.com>
2014-04-10 21:53:29 +0000

    code reformatting

--- rdftools/datagen/lubm_uni2many.py
+++ rdftools/datagen/lubm_uni2many.py
@@ -1,7 +1,12 @@
 from base import LubmGenerator
+import numpy as np
 
 __author__ = 'basca'
 
+def weighted_values(values, probabilities, size):
+    bins = np.add.accumulate(probabilities)
+    return values[np.digitize(np.random.random_sample(size), bins)]
+
 class LubmUni2Many(LubmGenerator):
     def _create_distribution(self, universities_rdf):
-        pass
+        np.random.random_integers(0, self._sites-1, len(universities_rdf))
