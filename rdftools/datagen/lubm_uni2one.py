1f8973faaf191d06ad3e404ff654bd0126930ffb
Cosmin Basca <cosmin.basca@gmail.com>
2014-04-10 15:29:45 +0000

    bumped to version 0.3.11
    - better tool framework now

--- rdftools/datagen/lubm_uni2one.py
+++ rdftools/datagen/lubm_uni2one.py
@@ -10,11 +10,12 @@ class LubmUni2One(LubmGenerator):
         sites_index = np.random.random_integers(0, self._sites-1, len(universities_rdf))
         print 'site index = ', sites_index
         for i in xrange(self._sites):
-            unis = sites_index[sites_index == i]
+            unis = np.where(sites_index == i)
+            print 'site %s'%i
+            print '\tunis = ',unis
             if unis.size > 0:
-                with io.open(self.site_path(i), mode='w+', buffering=1024 * 1024 * 16) as SITE:
+                with io.open(self.site_path(i), mode='w+') as SITE:
                     for uni_rdf in [universities_rdf[idx] for idx in unis]:
-                        with io.open(uni_rdf, mode='r', buffering=1024 * 1024 * 16) as UNI:
-                            triples = UNI.read()
-                        SITE.write(triples)
+                        with io.open(uni_rdf, mode='r') as UNI:
+                            SITE.write(UNI.read())
 
